b8ad79a24a1aecc2df3dc2a88c5c023eceecea8e
rina-reimer <rinareimer616@gmail.com>
2024-04-12 22:56:12 -0700

    more updates to translation

--- js/main.js
+++ js/main.js
@@ -48,29 +48,34 @@ class Translate {
 		this.lng = lng;
 	}
 	// translate 
-	process() {
-		var attr = this.attribute;
-		var xrhFile = new XMLHttpRequest();
-		// load content data 
-		xrhFile.open("GET", "https://rinareimer.com/" + this.lng + ".json", false);
-		xrhFile.onreadystatechange = function () {
-			if (xrhFile.readyState === 4) {
-				if (xrhFile.status === 200 || xrhFile.status == 0) {
-					var LngObject = JSON.parse(xrhFile.responseText);
-					var allDom = document.getElementsByTagName("*");
-					for (var i = 0; i < allDom.length; i++) {
-						var elem = allDom[i];
-						var key = elem.getAttribute(attr);
-						if (key != null) { 
-							elem.innerHTML = LngObject[key];
-						}
-					}
-
-				}
-			}
-		};
-		xrhFile.send();
-	};
+process() {
+  var attr = this.attribute;
+  var xrhFile = new XMLHttpRequest();
+  // load content data 
+  xrhFile.open("GET", "https://rinareimer.com/lng/" + this.lng + ".json", false);
+  console.log("got translate");
+  xrhFile.onreadystatechange = function () {
+    if (xrhFile.readyState === 4) {
+      if (xrhFile.status === 200 || xrhFile.status == 0) {
+        var LngObject = JSON.parse(xrhFile.responseText);
+        var allDom = document.getElementsByTagName("*");
+        for (var i = 0; i < allDom.length; i++) {
+          var elem = allDom[i];
+          var key = elem.getAttribute(attr);
+          if (key != null) { 
+            elem.innerHTML = LngObject[key];
+          }
+        }
+      } else {
+        console.error("Failed to load content data: " + xrhFile.status);
+      }
+    }
+  };
+  xrhFile.onerror = function() {
+    console.error("Failed to load content data.");
+  };
+  xrhFile.send();
+};
 }
 
 // This function will be called when user click changing language
